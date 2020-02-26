843582d9bb783aebd06b9912e0ca398e833a770e
jantapabh <jantapa2407@gmail.com>
2020-02-27 03:44:55 +0700

    commit

--- my-app/src/Bear.js
+++ my-app/src/Bear.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios'
+
 
 
 
 const Bear = () => {
+
+  const [bears , setBears] = useState({});
   
-  return (
+  const getBear = async () => {
+   
+  const result =  await axios.get('http://localhost:8000/api/bears')
+  console.log(result);
+
+  }
+
+  //useEffect เอาไว้เรียกใช้เมื่อ aysnc await render เรียบร้อยแล้ว
+
+useEffect(() => {
+
+  getBear()
+  console.log('If call');
+
+
+
+})
+
+    return (
     
     <div>
+   
    Bear
+  {/* <h1> {getBear()} </h1> */}
 
     </div>
   
