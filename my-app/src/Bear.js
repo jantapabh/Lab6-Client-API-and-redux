20f2690f77a4e4a351f680b8d35360086f24b8ec
jantapabh <jantapa2407@gmail.com>
2020-02-27 03:29:50 +0700

    add Fontend react

---
+++ my-app/src/Bear.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+const Bear = () => {
+  
+  return (
+    
+    <div>
+   Bear
+
+    </div>
+  
+  );
+}
+
+export default Bear;
