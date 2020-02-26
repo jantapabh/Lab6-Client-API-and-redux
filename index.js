13eaecc593c353a8ec4fd962fae345cf492aba08
jantapabh <jantapa2407@gmail.com>
2020-02-27 02:39:37 +0700

    commit modules

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-let express = reqiure('express')
+let express = require('express');
+let bodyParser = require('body-parser');
+let router = express.Router();
+let cors = require('cors');
 let app = express();
+app.use(cors());
 
-app.get('/', (req, res) => {
+// all of our routes will be prefixed with /api
+app.use('/api', bodyParser.json(), router);   //[use json]
+app.use('/api', bodyParser.urlencoded({ extended: false }), router);
 
-   res.send("Hi");
+let bears = [{ 
+    'id': 0, 'name': 'pooh', 'weight': 211 },
+    { 'id': 1, 'name': 'vinnie', 'weight': 111 }
+];
 
-})
+router.route('/bears').get((req, res) => res.json(bears));
 
-app.listen(8000,  () => console.log("Server is running") );
+app.use("*", (req, res) => res.status(404).send('404 Not found'));
+app.listen(8000, () => console.log("Server is running"));
