d217175dbaa1be1b2c01dc4ed0fca61cdba20c9a
jantapabh <jantapa2407@gmail.com>
2020-02-27 03:17:25 +0700

    commit

--- index.js
+++ index.js
@@ -9,12 +9,62 @@ app.use(cors());
 app.use('/api', bodyParser.json(), router);   //[use json]
 app.use('/api', bodyParser.urlencoded({ extended: false }), router);
 
-let bears = [{ 
-    'id': 0, 'name': 'pooh', 'weight': 211 },
-    { 'id': 1, 'name': 'vinnie', 'weight': 111 }
+let bears = [{
+    'id': 0, 'name': 'pooh', 'weight': 211
+},
+{ 'id': 1, 'name': 'vinnie', 'weight': 111 }
 ];
 
-router.route('/bears').get((req, res) => res.json(bears));
+router.route('/bears')
+
+    .get((req, res) => res.json(bears))
+    //ขอค่าจาก  bear ใช้ get
+
+    .post((req, res) => {
+
+        let bear = {}
+
+        bear.id = bears[bears.length - 1].id + 1;
+        bear.name = req.body.name;
+        bear.weight = req.body.weight;
+        bears.push(bear);
+        res.json({ message: 'Bear Create Finish' })
+
+        // ส่วนการเพิ่มข้อมูลใช้ post
+
+
+    })
+
+router.route('/bears/:bear_id')
+
+    .get((req, res) => {
+
+        let id = req.params.bear_id
+        let index = bears.findIndex(bear => (bear.id === +id))
+        res.json(bears[index])
+
+    })
+
+    .put((req, res) => {
+        // Update a bear
+        let id = req.params.bear_id
+        let index = bears.findIndex(bear => (bear.id === +id))
+        bears[index].name = req.body.name;
+        bears[index].weight = req.body.weight;
+        res.json({ message: 'Bear updated!' + req.params.bear_id });
+    })
+
+    .delete((req, res) => {                 
+        // Delete a bear
+        // delete     bears[req.params.bear_id]
+        let id = req.params.bear_id
+        let index = bears.findIndex(bear => bear.id === +id)
+        bears.splice(index, 1)
+        res.json({ message: 'Bear deleted: ' + req.params.bear_id });
+    })
+
 
 app.use("*", (req, res) => res.status(404).send('404 Not found'));
 app.listen(8000, () => console.log("Server is running"));
+
+
