75deeca7b5c0c87d484c3d6dc52ccea850b3ebc2
jantapabh <jantapa2407@gmail.com>
2020-02-27 12:56:18 +0700

    commit

--- my-app/src/Bear.js
+++ my-app/src/Bear.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Axios from 'axios'
@@ -8,35 +8,89 @@ import Axios from 'axios'
 
 const Bear = () => {
 
-  const [bears , setBears] = useState({});
-  
+  const [bears, setBears] = useState({});
+  const [name, setName] = useState('')
+  const [weight, setWeight] = useState(0);
+
+
   const getBear = async () => {
-   
-  const result =  await axios.get('http://localhost:8000/api/bears')
-  console.log(result);
+
+    const result = await axios.get('http://localhost:8000/api/bears')
+    console.log(result);
+    setBears(result.data)
+
 
   }
 
   //useEffect เอาไว้เรียกใช้เมื่อ aysnc await render เรียบร้อยแล้ว
 
-useEffect(() => {
+  useEffect(() => {
 
-  getBear()
-  console.log('If call');
+    getBear();
+    console.log('UseEffect is running');
+
+  })
+
+  const getBears = async () => {
+
+    const result = await axios.get('http://localhost:8000/api/bears', {
+
+       name,
+       weight
+
+    })
+
+    setName(result.data.name)
+    setWeight(result.data.weight)
+    getBear();
+
+
+  }
+
+  const addtBears = () => {
 
 
 
-})
 
-    return (
-    
+  }
+  const deleteBears = () => {
+
+
+
+
+  }
+  const updatesBears = () => {
+
+
+
+
+  }
+
+  const printBears = (id) => {
+
+    if (bears && bears.length)
+
+      return bears.map((bear, index) => {
+
+        return (
+
+          <li key={index}>
+             {bear.name} : {bear.weight}
+          <button onClick={() => getBear(bear.id)}> Get </button>
+          </li>
+        )
+      })
+
+  }
+
+
+  // ส่วน render ขึ้น
+  return (
+
     <div>
-   
-   Bear
-  {/* <h1> {getBear()} </h1> */}
 
     </div>
-  
+
   );
 }
 
