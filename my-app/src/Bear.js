11f2eb445f5f2653d8a5647db82c1da375401ca8
jantapabh <jantapa2407@gmail.com>
2020-02-27 15:15:37 +0700

    commit

--- my-app/src/Bear.js
+++ my-app/src/Bear.js
@@ -1,135 +1,232 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Axios from 'axios'
+// import React, { useState, useEffect } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import Axios from 'axios'
 
-var axios = require('axios')
-
-
-const Bear = () => {
-
-  const [bears, setBears] = useState({});
-  const [name, setName] = useState('')
-  const [weight, setWeight] = useState(0);
+// var axios = require('axios')
 
 
+// const Bear = () => {
 
-  //useEffect เอาไว้เรียกใช้เมื่อ aysnc await render เรียบร้อยแล้ว
-
-  useEffect(() => {
-
-    getBear();
-    console.log('UseEffect is running');
-
-  })
-
-  const getBear = async () => {
-
-    const result = await axios.get('http://localhost:8000/api/bears')
-    console.log(result);
-    setBears(result.data)
+//   const [bears, setBears] = useState({});
+//   const [name, setName] = useState('')
+//   const [weight, setWeight] = useState(0);
 
 
-  }
 
-  const getBears = async (id) => {
+//   //useEffect เอาไว้เรียกใช้เมื่อ aysnc await render เรียบร้อยแล้ว
 
-    const result = await axios.get(`http://localhost/api/bears/${id}`)
+//   useEffect(() => {
 
-    console.log(result.data)
-    setName(result.data.name)
-    setWeight(result.data.weight)
+//     getBear();
+//     console.log('UseEffect is running');
 
+//   })
 
-  }
+//   const getBear = async () => {
 
-  const addBears = async () => {
-
-    const result = await axios.post(`http://localhost/api/bears`, {
-      name,
-      weight
-    })
-
-    console.log(result.data)
-    getBear()
-  }
+//     const result = await axios.get('http://localhost:8000/api/bears')
+//     console.log(result);
+//     setBears(result.data)
 
 
-  const deleteBears = async (id) => {
+//   }
 
-    const result = await axios.delete(`http://localhost/api/bears/${id}`)
-    getBear()
+//   const getBears = async (id) => {
+
+//     const result = await axios.get(`http://localhost/api/bears/${id}`)
+
+//     console.log(result.data)
+//     setName(result.data.name)
+//     setWeight(result.data.weight)
 
 
-  }
-  const updatesBears = async (id) => {
+//   }
 
-    const result = await axios.put(`http://localhost/api/bears/${id}`, {
-      name,
-      weight
-    })
+//   const addBears = async () => {
 
-    console.log(result.data)
-    setName(result.data.name)
-    setWeight(result.data.weight)
-    getBear()
+//     const result = await axios.post(`http://localhost/api/bears`, {
+//       name,
+//       weight
+//     })
 
-  }
+//     console.log(result.data)
+//     getBear()
+//   }
 
-  const printBears = async () => {
 
-    if (bears && bears.length)
+//   const deleteBears = async (id) => {
 
-      return bears.map((bear, index) => {
+//     const result = await axios.delete(`http://localhost/api/bears/${id}`)
+//     getBear()
 
-        return (
 
-          <li key={index}>
-            {bear.name} : {bear.weight} <br />
-            <button onClick={() => getBears(bear.id)}>Get</button> <br />
-            <button onClick={() => deleteBears(bear.id)}>Del</button><br />
-            <button onClick={() => updatesBears(bear.id)}>Update</button> <br />
-          </li>
-        )
-      })
-    else {
+//   }
+//   const updatesBears = async (id) => {
 
-      return (<h2> No bear </h2>)
+//     const result = await axios.put(`http://localhost/api/bears/${id}`, {
+//       name,
+//       weight
+//     })
+
+//     console.log(result.data)
+//     setName(result.data.name)
+//     setWeight(result.data.weight)
+//     getBear()
+
+//   }
+
+//   const printBears = async () => {
+
+//     if (bears && bears.length)
+
+//       return bears.map((bear, index) => {
+
+//         return (
+
+//           <li key={index}>
+//             {bear.name} : {bear.weight} <br />
+//             <button onClick={() => getBears(bear.id)}>Get</button> <br />
+//             <button onClick={() => deleteBears(bear.id)}>Del</button><br />
+//             <button onClick={() => updatesBears(bear.id)}>Update</button> <br />
+//           </li>
+//         )
+//       })
+//     else {
+
+//       return (<h2> No bear </h2>)
+//     }
+
+//   }
+
+
+//   // ส่วน render ขึ้น
+//   return (
+
+//     <div>
+//       Bear
+//     <ul>
+//         {printBears()}
+//     </ul>
+
+//       <h2>Get Bear</h2>
+//       Get: {name} : {weight}
+
+//       <h2>Add Bear</h2>
+//       Name:
+//     <input
+//         placeholder="name"
+//         type="text"
+//         name="name"
+//         onChange={(e) => setName(e.target.value)}
+//       /> <br />
+//       Weight:
+//     <input
+//         type="number"
+//         name="weight"
+//         onChange={(e) => setWeight(e.target.value)}
+//       /><br />
+//       <button onClick={addBears}>Add </button>
+//     </div>
+
+//   );
+// }
+
+// export default Bear;
+
+import React, {useState, useEffect}  from 'react'
+import axios from 'axios'
+
+export default () => {
+
+    const [bears, setBears] = useState({}) 
+    const [name, setName] = useState('') 
+    const [weight, setWeight] = useState(0) 
+
+    useEffect( () => {
+        getBears()
+    } , [] )
+
+    const getBears = async () => {
+       const result = await axios.get(`http://localhost:8000/api/bears`)
+       console.log(result.data)
+       setBears(result.data)
     }
 
-  }
+    const addBear = async () => {
+        const result = await axios.post(`http://localhost:8000/api/bears`, {
+            name,
+            weight
+        })
+        console.log(result.data)
+        getBears()
+    }
 
+    const getBear = async (id) => {
+        const result = await axios.get(`http://localhost:8000/api/bears/${id}`)
+        console.log(result.data)
+        setName(result.data.name)
+        setWeight(result.data.weight)        
+    }
+    const updateBear = async (id) => {
+        const result = await axios.put(`http://localhost:8000/api/bears/${id}`,{
+            name,
+            weight
+        })
 
-  // ส่วน render ขึ้น
-  return (
+        console.log(result.data)
+        setName(result.data.name)
+        setWeight(result.data.weight)  
+        getBears()      
+    }
 
-    <div>
-      Bear
-    <ul>
-        {printBears()}
-    </ul>
+    const delBear = async (id) => {
+        const result = await axios.delete(`http://localhost:8000/api/bears/${id}`)       
+        getBears()
+    }
 
-      <h2>Get Bear</h2>
-      Get: {name} : {weight}
+    const printBears = () => {
+        if ( bears && bears.length )
+            return bears.map((bear,index) => {
+                return (
+                    <li key={index}>
+                        {bear.name} : {bear.weight}
+                        <button onClick={() => getBear(bear.id)}> Get </button>
+                        <button onClick={() => delBear(bear.id)}> Del </button>
+                        <button onClick={() => updateBear(bear.id)}> Update </button>
+                    </li>
+                )
+            })
+        else {
+            return (<h2> No bear </h2>)
+        }
 
-      <h2>Add Bear</h2>
-      Name:
-    <input
-        placeholder="name"
-        type="text"
-        name="name"
-        onChange={(e) => setName(e.target.value)}
-      /> <br />
-      Weight:
-    <input
-        type="number"
-        name="weight"
-        onChange={(e) => setWeight(e.target.value)}
-      /><br />
-      <button onClick={addBears}>Add </button>
-    </div>
+    }
 
-  );
+    return (
+        <div>
+            Bear
+            <ul>
+                 {printBears()}
+            </ul>
+            <h2>Get Bear</h2>
+            Get: {name} : {weight}
+
+            <h2>Add Bear</h2>
+            Name: 
+            <input 
+                placeholder="name"
+                type="text"
+                name="name"
+                onChange={ (e)=> setName(e.target.value) }
+                /> <br/>
+            Weight:
+            <input                 
+                type="number"
+                name="weight"
+                onChange={ (e)=> setWeight(e.target.value) }
+                /><br/>
+            <button onClick={addBear}>Add </button>
+      </div>
+    )
 }
-
-export default Bear;
